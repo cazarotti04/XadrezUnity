709430d4d0dc3574e88b4268da44e44d181589f9
Alexandre Miranda <alexandre.mm@outlook.com>
2016-11-06 15:04:05 -0200

    Acrescentando o codigo do projeto xadrez

---
+++ Assets/Scripts/tabuleiro/Cor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
