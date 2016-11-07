004d897b3cd84351c582c72f35333a643cfacf5e
LUCAS FERREIRA RESENDE <12409214622@est.udicentro.local>
2016-11-07 12:05:51 -0200

    Ajustando o rei branco e incluindo rei preto

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,11 +5,13 @@ using xadrez;
 class GameController : MonoBehaviour {
 
     public GameObject reiBranco = null;
+    public GameObject reiPreto = null;
 
     PartidaDeXadrez partida;
 
 	void Start () {
         partida = new PartidaDeXadrez();
         Util.instanciarRei('e', 1, Cor.Branca, partida, reiBranco);
-	}
+        Util.instanciarRei('e', 8, Cor.Preta, partida, reiPreto);
+    }
 }
