3484c6f77361dc34313d65ada6819d85a208cc71
LUCAS FERREIRA RESENDE <12409214622@est.udicentro.local>
2016-11-07 11:21:53 -0200

    Instanciando o rei branco na cena

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using tabuleiro;
+using xadrez;
+
+class GameController : MonoBehaviour {
+
+    public GameObject reiBranco = null;
+
+    PartidaDeXadrez partida;
+
+	void Start () {
+        partida = new PartidaDeXadrez();
+        Util.instanciarRei('e', 1, Cor.Branca, partida, reiBranco);
+	}
+}
